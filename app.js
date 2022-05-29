34ea4d3bd47b809066e10959713e7bcfcb933435
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-29 13:19:12 +0800

    read json file

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 
+const cats = JSON.parse(String(fs.readFileSync(`${__dirname}/./dev-data/data/cat-image-list.json`)));
 
+console.log(cats);
 
 const port = 3000;
 app.listen(port, () => {
